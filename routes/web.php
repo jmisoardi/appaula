c94315ccc3811b128b99a766fe81b1f937e62db2
jair <87211745+jmisoardi@users.noreply.github.com>
2024-09-01 00:54:12 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,27 @@
 <?php
 
-use App\Http\Controllers\CursoController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\CursoController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\HomeController;
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
-
+    /* En este caso tenemos el llamado de una unica ruta por HomeController. En la carpeta controlador HomeController, posee el metodo invoke */
     route::get('/', HomeController::class);
-
+    
+    /* En este caso tememos rutas individuales con el mismo controlador llamando a distintas rutas index, create, show. */
     route::get('curso', [CursoController::class, 'index'] );
-
     Route::get('curso/create', [CursoController::class, 'create'] );
-
     /* Creación de una variable por Url */
     Route::get('curso/{curso}',  [CursoController::class, 'show'] );
+
+    /* En este caso estamos agrupando las rutas con el mismo controlador, CursoController, para tener el codigo más limpio */
+    Route::controller(CursoController::class)->group(function(){
+        route::get('curso', 'index');
+        route::get('curso/create','create');
+        route::get('curso/{curso}', 'show');
+    });
 
     /* Route::get('curso/{curso}/{categoria?}', function ($curso,$categoria = null) {
         
